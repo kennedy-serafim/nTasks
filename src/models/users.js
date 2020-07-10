842e72370011b9356b806f19caf60d6683cb2784
kennedy-serafim <serafimkennedy@gmail.com>
2020-07-10 12:57:43 +0200

    Atualizando o model user inserindo o password hash

--- src/models/users.js
+++ src/models/users.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt';
+
 module.exports = (sequelize, DataTypes) => {
     const Users = sequelize.define('Users', {
         id: {
@@ -18,15 +20,31 @@ module.exports = (sequelize, DataTypes) => {
             validate: {
                 notEmpty: true
             }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
         }
     }, {
+        hooks: {
+            beforeCreate: user => {
+                const salt = bcrypt.genSaltSync();
+                user.password = bcrypt.hashSync(user.password, salt);
+            }
+        },
+
         classMethods: {
             associate: (models) => {
                 Users.hasMany(models.Tasks);
+            },
+            isPassword: (encodedPassword, password) => {
+                return bcrypt.compareSync(password, encodedPassword);
             }
         }
-    }
-    );
+    });
 
     return Users;
 }
