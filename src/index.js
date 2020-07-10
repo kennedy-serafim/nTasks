d49cf505654af1e3fa48c20e3ab9da0f128e1fdc
kennedy-serafim <serafimkennedy@gmail.com>
2020-07-10 12:50:11 +0200

    Configurando o auth

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ consign({
 })
     .include('libs/config.js')
     .then('data.js')
+    .then('auth.js')
     .then('libs/middlewares.js')
     .then('routes')
     .then('libs/boot.js')
--- src/libs/middlewares.js
+++ src/libs/middlewares.js
@@ -1,6 +1,7 @@
 module.exports = app => {
     app.set('port', 3030);
     app.set('json spaces', 4);
+    app.use(app.auth.initialize());
     app.use((req, res, next) => {
         next();
     });
