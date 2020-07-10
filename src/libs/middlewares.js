67d21661eca96f118a412f271a250e6206748c4d
kennedy-serafim <serafimkennedy@gmail.com>
2020-07-10 10:47:00 +0200

    Tasks route

--- src/libs/middlewares.js
+++ src/libs/middlewares.js
@@ -1,4 +1,8 @@
 module.exports = app => {
     app.set('port', 3030);
     app.set('json spaces', 4);
+    //  app.use(express.json());
+    app.use((req, res, next) => {
+        next();
+    });
 }
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,9 +1,48 @@
 module.exports = app => {
     const Tasks = app.data.Tasks;
 
-    app.get('/tasks', (req, res) => {
-        Tasks.findAll({}).then(tasks => {
-            res.json(tasks);
+    app.route("/tasks")
+        .get((req, res) => {
+            //Listar tarefas
+            Tasks.findAll({})
+                .then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json(error.message);
+                });
+        })
+        .post((req, res) => {
+            Tasks.create(req.body)
+                .then(result => req.json(result))
+                .catch(err => {
+                    res.status(412).json(err.message);
+                });
         });
-    });
+
+    app.route('/tasks/:id')
+        .get((req, res) => {
+            Tasks.findOne({ where: req.params })
+                .then(result => {
+                    if (result) {
+                        res.json(result);
+                    } else {
+                        res.status(404).json({ message: 'Tasks not found' });
+                    }
+                }).catch(err => {
+                    res.status(412).json(err.message);
+                });
+        })
+        .put((req, res) => {
+            Tasks.update(req.body, { where: req.params })
+                .then(result => res.sendStatus(204))
+                .catch(err => {
+                    res.status(412).json(err.message);
+                });
+        })
+        .delete((req, res) => {
+            Tasks.destroy({ where: req.params })
+                .then(result => res.sendStatus(204))
+                .catch(err => {
+                    res.status(412).json(err.message);
+                });
+        });
 }
